8efd1439fa9fcd8c79a91cbf5ac5168734816b1b
ihab Youssef <pixeltagnet@gmail.com>
2019-03-29 16:04:06 +0200

    recap and add complete todo

--- src/component/todo.jsx
+++ src/component/todo.jsx
@@ -5,14 +5,17 @@ export default class todo extends Component {
         todo : this.props.todo
     };
     render() {
-        let todoTitle = this.state.todo.title;
+        const todo = this.state.todo;
+        const unCompeletedClass = "media text-muted pt-3"
+        const compeletedClass = "media text-muted pt-3 completed-todo"
         return (
             <React.Fragment>
-                <div className="media text-muted pt-3">
+                <div className={ todo.done ? compeletedClass : unCompeletedClass }>
                     <svg className="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-                    <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                        {todoTitle}
-                    </p>
+                    <h6 className="media-body">
+                        {todo.value}
+                    </h6>
+                    <button className="btn btn-default" onClick={() => this.props.onComplete(todo.id)  }>Complete</button>
                 </div>
             </React.Fragment>
         )
--- src/component/todoApp.jsx
+++ src/component/todoApp.jsx
@@ -4,26 +4,54 @@ import Todo from './todo'
 export default class todoApp extends Component {
 
     state = {
-        todos : [ ],
-        newTodo :  {id : Number , title : '' , selected : Boolean, done : Boolean }
+        todos : [],
+        newTodo : ''
     };
     render() {
         return (
             <div className="my-3 p-3 bg-white rounded shadow-sm">
                 <h6 className="border-bottom border-gray pb-2 mb-0">Todo</h6>
-                <input type="text" value={this.state.newTodo.title} onChange={(event) => {this.addNewTodo(event)}} onKeyPress={(event) => {this.addNewTodo(event)}} placeholder="Enter New Todo" />
-                { this.state.todos.map(todo =>    <Todo key={todo.id} todo={todo} />   ) }
+                <input type="text" className="form-control form-control-lg" value={this.state.newTodo} onChange={(event) => {this.addNewTodo(event)}} onKeyPress={(event) => {this.addNewTodo(event)}} placeholder="Enter New Todo" />
+                { this.state.todos.map(todo =>    <Todo key={todo.id} todo={todo} onComplete={this.handleComplete} />   ) }
             </div>
         )
     }
 
+    // add new Todo
     addNewTodo = (e) => {
+        // when Enter add new todo
         if(e.key === 'Enter') {
-            this.setState({todos : this.state.todos.concat({id : Number(new Date()), title : e.target.value , selected : false, done : false })})
-            this.setState({newTodo  : {id : Number(new Date()) , title : '' , selected : false, done : false }})
-            console.log(this.state.todos);
+            if(e.target.value !== '') {
+                const newTodo = {
+                    id: Number(new Date()),
+                    value: this.state.newTodo.slice(),
+                    selected : false,
+                    done : false
+                  };
+    
+                const todos = [...this.state.todos];
+                todos.push(newTodo);
+                this.setState({todos , newTodo : ''})
+                
+            }
         } else {
-            this.setState({newTodo : {id : Number(new Date()) , title : e.target.value , selected : false, done : false }})
+            // handling on Change
+            this.setState({ newTodo : e.target.value})
         }
     }
+
+    // mark todo as Done
+    
+    handleComplete = (todo) => {
+        let todos = this.state.todos.map( c => {
+            if(c.id === todo) {
+                c.done = true;
+                return c; 
+            }
+            return c;
+        });
+        this.setState({todos});
+    }
+
+    
 }
